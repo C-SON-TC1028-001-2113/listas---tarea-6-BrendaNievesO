8e699674369734542f1b0f808aaeb3bc9d7cc092
BrendaNievesO <a01252478@itesm.mx>
2021-10-05 19:23:22 +0000

    t6

--- assignments/09SerieFibonacciListas/src/exercise.py
+++ assignments/09SerieFibonacciListas/src/exercise.py
@@ -1,6 +1,22 @@
+
 def main():
-    
-    pass
+    lista=[0,1]
+    a=0
+    b=1
+    v=int(input(''))
+    c=v-2
+    if v==1:
+        lista.remove(1)
+        print(lista)
+    if v==2:
+        print(lista)
+    if v>2:
+        for x in range(c):
+            lista.append(a+b)
+            s=a+b
+            a=b
+            b=s
+        print(lista)
 
 if __name__=='__main__':
     main()
